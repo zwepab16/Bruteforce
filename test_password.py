c29692eaf9c61533ac55bc454b39ca68643fb2cf
zwepab16 <zwepab16@htlkaindorf.at>
2019-03-25 11:28:26 +0100

    tavis file erstellt

--- test_password.py
+++ test_password.py
@@ -8,4 +8,4 @@ class TestPassword(TestCase):
 
     def test_check2(self):
         pwd=Password("abc")
-        self.assertEqual(True,pwd.check("bddb"))
+        self.assertEqual(False,pwd.check("bddb"))
